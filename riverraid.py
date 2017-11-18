c96e611ad4c6d9671dfac2d6ca49e223ad211793
Prakash, Karthik <Karthik_Prakash2@intuit.com>
2017-11-17 20:09:55 -0800

    preprcoessing done the right way

--- riverraid.py
+++ riverraid.py
@@ -1,3 +1,4 @@
+from numpy import random
 from numpy.core.numeric import ndarray
 from scipy.misc.pilutil import imresize
 
@@ -12,6 +13,8 @@ import scipy.misc
 
 C = 10000
 N = 1000000
+k = 4
+
 
 def initNet():
     model = Sequential()
@@ -26,51 +29,91 @@ def initNet():
     return model
 
 def QFunc(model, phi, action):
-    
+
     pass
 
-def preprocess(prev, curr):
-    def step1(prev, curr):
-        maxObservation = ndarray(prev.shape)
-        for i in range(prev.shape[0]):
-            for j in range(prev.shape[1]):
-                rgb = prev[i][j]
+
+def preprocess(recentObservations):
+    def getMaxBetweenTwo(ob1, ob2):
+        maxObservation = ndarray(ob1.shape)
+        for i in range(ob1.shape[0]):
+            for j in range(ob1.shape[1]):
+                rgb1 = ob1[i][j]
+                rgb2 = ob2[i][j]
                 maxObservation[i][j] = (
-                max(prev[i][j][0], curr[i][j][0]), max(prev[i][j][1], curr[i][j][1]),
-                max(prev[i][j][2], curr[i][j][2]))
+                    max(rgb1[0], rgb2[0]),
+                    max(rgb1[1], rgb2[1]),
+                    max(rgb1[2], rgb2[2]))
         return maxObservation
 
-    def getYChannel(observation):
+    def step1():
+        maxObservations = []
+        for i in range(k):
+            maxObservations.append(getMaxBetweenTwo(recentObservations[i], recentObservations[i+1]))
+        return maxObservations
+
+
+    def getYChannelForOneObservation(ob):
         yData = ndarray((210, 160))
-        for i in range(observation.shape[0]):
-            for j in range(observation.shape[1]):
-                rgb = observation[i][j]
+        for i in range(ob.shape[0]):
+            for j in range(ob.shape[1]):
+                rgb = ob[i][j]
                 yData[i][j] = 0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]
         return yData
 
-    def step2(img):
-        return imresize(img, (84, 84))
+    def getYChannelsForAllObservations(maxObservations):
+        yChannels = []
+        for ob in maxObservations:
+            yChannels.append(ob)
+        return yChannels
 
-    maxObservation = step1(prev, curr)
-    yChannel = getYChannel(maxObservation)
-    return step2(yChannel)
+    def step2(yChannels):
+        preprocessedImage = ndarray((84,84,4))
+        for imgCounter in range(len(yChannels)):
+            # TODO: look into bilinear reduction
+            img = imresize(yChannels[imgCounter], (84, 84))
+            for i in range(img.shape[0]):
+                for j in range(img.shape[1]):
+                    preprocessedImage[i][j][imgCounter] = img[i][j]
+        return preprocessedImage
+
+    return step2(getYChannelsForAllObservations(step1(recentObservations)))
 
 def DQN():
     Ccounter = 0
+    kCounter = 0
     env = gym.make('Riverraid-v0')
     Q = initNet()
     QHat = initNet()
     average=0
     num_episodes=1
+    epsilon = 0.1
+    recentKObservations= []
     D = []
     for i_episode in range(num_episodes):
         total_reward = 0
         observation = env.reset()
         prevObservation = observation
+        action = None
         for t in range(1000000):
             env.render()
+            kCounter += 1
+            if kCounter == 2*k:
+                # select new action based on epsilon greedy solution
+                if random.uniform(0, 1) < epsilon:
+                    action = env.action_space.sample()
+                else:
+                    phi = preprocess()
+                    action = action
+            else:
+                # repeat the past selected action on the environment
+                # this is done to make the processing faster
+                recentKObservations.append(observation)
+                pass
+
             # TODO: Perform epsilon greedy selection of actions
-            action = env.action_space.sample()
+
+
             observation, reward, done, info = env.step(action)
             # Preprocess state to phi
             # TODO: preprocess needs to take a list of 8 frames but can be assumed for now
@@ -98,4 +141,5 @@ def DQN():
                 if Ccounter == C:
                     weights = Q.get_weights()
                     QHat.set_weights(weights)
+                    Ccounter = 0
     print "average reward={}".format(average/num_episodes)
